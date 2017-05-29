45ccf7f6c9772f87f8e3b1a406db0433c340f4ea
Coffeeteer <tgrieshiemer@gmail.com>
2017-05-29 18:21:56 -0400

    finished tutorial Insert and Get Data w/ MongoDB

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,54 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb');
+var assert = require('assert');
+
+var url = 'mongodb://localhost:27017/test';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Cool, huh!', condition: true, anyArray: [1,2,3] });
+  res.render('index');
 });
 
-router.get('/test/:id', function(req, res, next) {
-  res.render('test', {output: req.params.id});
+router.get('/get-data', function(req, res, next){
+	var resultArray = [];
+	mongo.connect(url, function(err, db){
+		assert.equal(null, err);
+		var cursor = db.collection('user-data').find();
+		cursor.forEach(function(doc, err){
+			assert.equal(null, err);
+			resultArray.push(doc);
+		}, function(){
+			db.close();
+			res.render('index', {items: resultArray});
+		});
+	});
 });
 
-router.post('/test/submit', function(req, res, next) {
-  res.redirect('/test/...');
+router.post('/insert', function(req, res, next){
+	var item = {
+		title: req.body.title,
+		content: req.body.content,
+		author: req.body.author
+	};
+
+	mongo.connect(url, function(err, db){
+		assert.equal(null, err);
+		db.collection('user-data').insertOne(item, function(err, result) {
+			assert.equal(null, err);
+			console.log('Item Inserted');
+			db.close();
+		});
+	});
+	res.redirect('/');
+});
+
+router.post('/update', function(req, res, next){
+
+});
+
+router.post('/post', function(req, res, next){
+
 });
 
 module.exports = router;
