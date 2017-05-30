38ed20b1d8bd4fa2e82fc1ac2d67bedfbddd3cb7
Coffeeteer <tgrieshiemer@gmail.com>
2017-05-30 10:16:02 -0400

    learned how to add, update, and delete in mongo db

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var mongo = require('mongodb');
+var mongo = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
 var assert = require('assert');
 
 var url = 'mongodb://localhost:27017/test';
@@ -44,11 +45,37 @@ router.post('/insert', function(req, res, next){
 });
 
 router.post('/update', function(req, res, next){
+	var item = {
+		title: req.body.title,
+		content: req.body.content,
+		author: req.body.author
+	};
 
+	var id = req.body.id;
+
+	mongo.connect(url, function(err, db){
+		assert.equal(null, err);
+		db.collection('user-data').updateOne({"_id": objectId(id)}, {$set: item}, function(err, result) {
+			assert.equal(null, err);
+			console.log('Item Update');
+			db.close();
+		});
+	});
+	res.redirect('/');
 });
 
-router.post('/post', function(req, res, next){
+router.post('/delete', function(req, res, next){
+	var id = req.body.id;
 
+	mongo.connect(url, function(err, db){
+		assert.equal(null, err);
+		db.collection('user-data').deleteOne({"_id": objectId(id)}, function(err, result) {
+			assert.equal(null, err);
+			console.log('Item Deleted');
+			db.close();
+		});
+	});
+	res.redirect('/');
 });
 
 module.exports = router;
